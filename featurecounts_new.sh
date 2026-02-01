d0d2eefe01a6bdd6da888a826f2cb511ca181fcc
Mariana-Cancian <70763791+Mariana-Cancian@users.noreply.github.com>
2026-02-01 15:21:51 -0300

    Update comments for clarity in featurecounts_new.sh

--- featurecounts_new.sh
+++ featurecounts_new.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-species_specific_gff="/media/labdros/Elements/RNAseq-mari/trimmed/star_align/dmel-all-r6.66.gtf"
+species_specific_gff="/media/labdros/Elements/RNAseq-mari/trimmed/star_align/dmel-all-r6.66.gtf" #GTF file location
 
-# lista todos os BAMs
+# list all BAM files
 bam_files=$(ls *.bam)
 
-# roda featureCounts em todos os BAMs de uma vez
+# run featureCounts on all BAM files at once
 featureCounts -a "$species_specific_gff" \
   -o all_samples_counts.txt \
   -p -B -C -T 20 -t exon -g gene_id -s 2\
