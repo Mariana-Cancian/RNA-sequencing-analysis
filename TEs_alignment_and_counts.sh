ffe143faff2cd07b96620bfdf3f263d5993c5419
Mariana-Cancian <70763791+Mariana-Cancian@users.noreply.github.com>
2026-02-01 15:39:49 -0300

    Rename alignment_and_counts.sh to TEs_alignment_and_counts.sh

---
+++ TEs_alignment_and_counts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+fq_files="Tub_Mos1_A
+Tub_Mos1_B
+Tub_Mos1_C
+Tub_w1118_A
+Tub_w1118_B
+Tub_w1118_C
+w1118_Mos1_A
+w1118_Mos1_B
+w1118_Mos1_C"
+
+while read -r sample; do
+    echo "$sample"
+    STAR --runThreadN 16 --genomeDir . \
+    --readFilesIn "$sample"_1_trim.fastq.gz "$sample"_2_trim.fastq.gz \
+    --outFilterMultimapNmax 100 --winAnchorMultimapNmax 100 \
+    --outFileNamePrefix "$sample" --outSAMtype BAM SortedByCoordinate --readFilesCommand gunzip -c
+
+    singularity exec tetranscripts.sif TEcount -b "$sample"Aligned.sortedByCoord.out.bam \
+    --GTF dmel-all-r6.49.gtf --TE dmel-r6.49_RM_fixed6.gtf \
+    --stranded reverse --sortByPos --mode multi --project "$sample"
+done <<< "$fq_files"
